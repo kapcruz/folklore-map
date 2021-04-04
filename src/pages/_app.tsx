c3009bd289191ebfb6524c06bd156e067ecbfa11
Karina <kapcruz@gmail.com>
2021-04-04 18:26:51 -0300

    feat: mapa basico do leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,21 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate</title>
+        <title>Mapa do folclore brasileiro</title>
+        <meta
+          name="description"
+          content="Um projeto que visa mostrar o mapa do folclore brasileiro"
+        />
+        <meta name="theme-color" content="#06092b" />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
         />
-        <meta name="theme-color" content="#06092b" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
